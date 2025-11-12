98abb2d86133e8d3c8c68131bd6ed52722bc80fb
eloise.gomez@dataiku.com <eloise.gomez@dataiku.com>
2025-11-12 17:02:53 +0000

    Saved recipe: _STARTER_EXTERNAL_COLLABORATION_WITH_GIT_AND_DSS_EXTERNAL_GIT_BENOIT.compute_t_shirt_data

--- recipes/compute_t_shirt_data.py
+++ recipes/compute_t_shirt_data.py
@@ -23,6 +23,7 @@ df = pd.read_csv(BytesIO(data))
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 df['total'] = df['tshirt_price'] * df['tshirt_quantity']
+df['total_100'] = df['tshirt_price'] * df['tshirt_quantity'] * 100
 
 # update text category description
 df['tshirt_category'] = df['tshirt_category'].str.replace('Wh ', 'White ')
